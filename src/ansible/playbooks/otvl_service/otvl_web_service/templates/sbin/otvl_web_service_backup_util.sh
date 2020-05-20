1f1ea3eacce3a5f4ba4869ae4689c4327ccc5bc8
thierry_otvl <thierry_otvl@otvl.org>
2020-05-20 17:56:01 +0200

    started testing future otvl_web_service role

---
+++ src/ansible/playbooks/otvl_service/otvl_web_service/templates/sbin/otvl_web_service_backup_util.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# otvl_web_service_backup_util.sh
+status=0
+
+otvl_sbin_path="{{ combined_otvl.config_paths.sbin }}"
+otvl_backup_path="{{ combined_otvl.config_paths.backup }}"
+
+exit $status
