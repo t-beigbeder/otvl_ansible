7d25a2a899446434669f7c1e695cf978292ea6fb
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-08-06 19:06:45 +0200

    fixed nongit asset config

--- src/ansible/playbooks/otvl_service/otvl_web_v4/deploy/host/tasks/templates/apache-custo-init-sync.sh
+++ src/ansible/playbooks/otvl_service/otvl_web_v4/deploy/host/tasks/templates/apache-custo-init-sync.sh
@@ -1,14 +1,12 @@
 echo $0 running $*
-echo env
-env | sort
 if [ "$OTVL_WEB_CABRI_SYNC" ]; then
-  if [ ! -d /data/obs_otvl_sites ] ; then
-    mkdir /data/obs_otvl_sites
-    echo "cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk $OVHSK obs:/data/obs_otvl_sites"
-    cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk $OVHSK obs:/data/obs_otvl_sites
+  if [ ! -d $HOME/obs_otvl_sites ] ; then
+    mkdir $HOME/obs_otvl_sites
+    echo "cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk $OVHSK obs:$HOME/obs_otvl_sites"
+    cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk $OVHSK obs:$HOME/obs_otvl_sites
   else
     echo "cabri cli dss unlock obs:/data/obs_otvl_sites"
-    cabri cli dss unlock obs:/data/obs_otvl_sites
+    cabri cli dss unlock obs:$HOME/obs_otvl_sites
   fi
 fi
 exec $*
