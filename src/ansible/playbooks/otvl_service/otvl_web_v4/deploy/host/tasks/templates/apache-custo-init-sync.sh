33177e17e0dc8db2a406d5d31741a877e34f42aa
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-08-15 16:34:12 +0200

    cool

--- src/ansible/playbooks/otvl_service/otvl_web_v4/deploy/host/tasks/templates/apache-custo-init-sync.sh
+++ src/ansible/playbooks/otvl_service/otvl_web_v4/deploy/host/tasks/templates/apache-custo-init-sync.sh
@@ -2,7 +2,7 @@ echo $0 running $*
 if [ "$OTVL_WEB_CABRI_SYNC" ]; then
   if [ ! -d $HOME/obs_otvl_sites ] ; then
     mkdir $HOME/obs_otvl_sites
-    echo "cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk $OVHSK obs:$HOME/obs_otvl_sites"
+    echo "cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk xxx obs:$HOME/obs_otvl_sites"
     cabri cli dss make --obsrg $OVHRG --obsep $OVHEP --obsct $OVHCT --obsak $OVHAK --obssk $OVHSK obs:$HOME/obs_otvl_sites
   else
     echo "cabri cli dss unlock obs:/data/obs_otvl_sites"
